931ec8f6471162348728ccde13e709b546e19723
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-01-05 18:20:06 -0600

    added questions variable

--- index.js
+++ index.js
@@ -4,4 +4,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-console.log(inquirer);
+// array of questions for user
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title!');
+                return false;
+            };
+        }
+    }, {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project',
+    }, {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions for the project',
+    }, {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information for the project',
+    }, {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution guidelines for the project',
+    }, {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions for the project',
+    }, {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for the project',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    }, {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username',
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+    }
+]
